90543f937cb94fa1a143939fdd2014ef05c78236
aapdo <jade04342@gmail.com>
2025-07-15 00:03:36 +0900

    change containerssh helm chart

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 
 # containerssh
-cd ~/admin_infra/containerssh
-kubectl apply -f k8s/ -n containerssh
+cd ~/admin_infra
+helm upgrade --install containerssh ./containerssh -n containerssh --create-namespace
 
 
 # auth server
 ## rebuild
-cd ~/admin_infra/containerssh-auth-server
+cd ~/admin_infra/auth-server
 docker build -t containerssh-auth-server:latest .
 
 ## apply k8s
