78bd465485f6adb6732e3876cf3bee746b388676
aapdo <jade04342@gmail.com>
2025-08-08 21:34:48 +0900

    change entrypoint.sh

--- guestImage/cuda11.8_cudnn8_dev_ubuntu22.04/entrypoint.sh
+++ guestImage/cuda11.8_cudnn8_dev_ubuntu22.04/entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -ex 
 
+if [[ -z "$USER_ID" || -z "$UID" ]]; then
+    echo "[ERROR] USER_ID or UID is not set"
+    exit 1
+fi
+
 echo "[INFO] Entrypoint started"
 echo "user_id: $USER_ID"
 echo "user_pw: $USER_PW"
@@ -11,19 +16,12 @@ echo "[INFO] entrypoint.sh 시작됨" >> /home/$USER_ID/entrypoint.log
 env >> /home/$USER_ID/entrypoint.log
 whoami >> /home/$USER_ID/entrypoint.log 2>&1
 
-sudo apt update
-sudo apt install -y auditd
-
-# /etc/audit/audit.rules 파일에 줄 추가
-# sed -i "/^#-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid=$USER_ID -k rm_commands" /etc/audit/audit.rules
-echo "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid=$USER_ID -k rm_commands" >> /etc/audit/audit.rules
-
-# history 명령어 칠 때 명령어를 입력한 시간이 같이 나오게 하는 명령어
-echo 'HISTTIMEFORMAT="[%Y-%m-%d %H:%M:%S] "' >> /etc/profile
-echo 'export HISTTIMEFORMAT' >> /etc/profile
 
 if ! id "$USER_ID" >/dev/null 2>&1; then
-    useradd -s /bin/bash -d /home/$USER_ID -u $UID $USER_ID
+    useradd -s /bin/bash -d /home/$USER_ID -u $UID $USER_ID || {
+        echo "[ERROR] useradd failed"
+        exit 1
+    }
 
     # sudo 권한 제공
     echo "$USER_ID ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
@@ -33,4 +31,5 @@ if ! id "$USER_ID" >/dev/null 2>&1; then
 fi
 
 #entrypoint.sh 를 실행하고 나서 컨테이너가 Exit 하지 않게함
-tail -F /dev/null
+#tail -F /dev/null
+exit 0
